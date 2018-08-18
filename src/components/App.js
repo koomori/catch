04c2245413da33f9e5c0dd43729c46672981ac64
Barbara Patten <=>
2018-08-18 13:00:15 -0500

    added AddFishForm and adding fish function

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,26 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends React.Component {
+	state = {
+		fishes: {},
+		order: {}
+	};
+
+	addFish = fish => {
+		console.log('adding a fish');
+		const fishes = { ...this.state.fishes };
+		fishes[`fish${Date.now()}`] = fish;
+		this.setState({
+			fishes: fishes
+		});
+	};
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market" age={500} cool={true} />
 				</div>
-				<Inventory />
+				<Inventory addFish={this.addFish} />
 				<Order />
 			</div>
 		);
